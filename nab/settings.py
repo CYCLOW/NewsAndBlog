104a712a79b14f44a185e8eb47142d283fc61e57
Tejas Arondekar <tejasarondekar1@gmail.com>
2021-01-22 14:32:58 +0530

    Tweaked For Heroku Hosting

--- nab/settings.py
+++ nab/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'nab.urls'
@@ -121,6 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
@@ -133,3 +139,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 't.arondekar@somaiya.edu'
 EMAIL_HOST_PASSWORD = '9987065334'
 EMAIL_HOST_PASS = ''
+
+django_heroku.settings(locals())
