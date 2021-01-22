33ae9f1693229d37021ce7251c4daed312a4b9de
Tejas Arondekar <tejasarondekar1@gmail.com>
2021-01-22 16:51:33 +0530

    Error Solving Attempt 5

--- theblog/urls.py
+++ theblog/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from .views import HomeView ,ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, NewsView
-from .models import New
 
 urlpatterns = [ 
 
